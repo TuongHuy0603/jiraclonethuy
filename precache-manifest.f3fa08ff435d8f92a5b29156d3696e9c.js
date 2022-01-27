7d8c8f16148bf1c6affa2c61e70555f49803bb8f
TuongHuy0603 <86156244+TuongHuy0603@users.noreply.github.com>
2022-01-27 17:16:44 +0700

    a
    up

---
+++ precache-manifest.f3fa08ff435d8f92a5b29156d3696e9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d21848040e7261ad34d6c8565d55d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e91c0779dbd5436150",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "5cba18ba1439b96f20a1",
+    "url": "/static/css/main.64e6e6ab.chunk.css"
+  },
+  {
+    "revision": "a4e91c0779dbd5436150",
+    "url": "/static/js/2.0f080f1d.chunk.js"
+  },
+  {
+    "revision": "8c082d53fa248d63e1aede82934af5da",
+    "url": "/static/js/2.0f080f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cba18ba1439b96f20a1",
+    "url": "/static/js/main.a2031e7c.chunk.js"
+  },
+  {
+    "revision": "ef624c4d4e219d7fb43e",
+    "url": "/static/js/runtime-main.c5285391.js"
+  },
+  {
+    "revision": "9b2b410b3894da9da9e86fea5832dafa",
+    "url": "/static/media/bg.9b2b410b.png"
+  },
+  {
+    "revision": "e96d3ef90db23e6aca4d9b13481ee1b4",
+    "url": "/static/media/download (1).e96d3ef9.jfif"
+  },
+  {
+    "revision": "1c8cee9f3c0c94be03f180afa6181f47",
+    "url": "/static/media/download.1c8cee9f.jfif"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/static/media/loading.504b535c.gif"
+  }
+]);
